df260668c7efc6f641a741a16a11e35b1fa72236
Corbin Champion <corbinlc@gmail.com>
2018-11-28 12:07:16 -0800

    specify size for the ecdsa to not conflict with other distros, probably should lock down all default sizes later

--- assets/all/startSSHServer.sh
+++ assets/all/startSSHServer.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 
-dropbear -E -R -p 2022
+if [ ! -d /etc/dropbear ]; then
+    mkdir /etc/dropbear
+fi
+
+if [ ! -f /etc/dropbear/dropbear_dss_host_key ]; then
+    dropbearkey -t dss -f /etc/dropbear/dropbear_dss_host_key
+fi
+
+if [ ! -f /etc/dropbear/dropbear_rsa_host_key ]; then
+    dropbearkey -t rsa -f /etc/dropbear/dropbear_rsa_host_key
+fi
+
+if [ ! -f /etc/dropbear/dropbear_ecdsa_host_key ]; then
+    dropbearkey -t ecdsa -s 521 -f /etc/dropbear/dropbear_ecdsa_host_key
+fi
+
+dropbear -E -p 2022
